4c8b7e441baa1be75903da04055b645b6adc68e8
Changhui Xu <changhuixu@hotmail.com>
2023-12-07 13:50:32 -0600

    update solution to .NET 8

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -1,45 +1,31 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MyWebApp.Models;
 
-namespace MyWebApp.Controllers
+namespace MyWebApp.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IMemoryCache _cache;
-        private readonly IHostApplicationLifetime _lifetime;
-        public HomeController(ILogger<HomeController> logger, IMemoryCache cache, IHostApplicationLifetime lifetime)
-        {
-            _logger = logger;
-            _cache = cache;
-            _lifetime = lifetime;
-        }
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [HttpPost]
-        public IActionResult Stop()
-        {
-            _lifetime.StopApplication();
-            return new ObjectResult("Application stopped successfully!");
-        }
+    [HttpPost]
+    public IActionResult Stop([FromServices] IHostApplicationLifetime lifetime)
+    {
+        lifetime.StopApplication();
+        return new ObjectResult("Application stopped successfully!");
     }
 }
--- MyWebApp/Startup.cs
+++ MyWebApp/Startup.cs
@@ -1,79 +1,61 @@
-using System;
-using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+namespace MyWebApp;
 
-namespace MyWebApp
+public class Startup(IHostEnvironment env)
 {
-    public class Startup
+    private string LogPath { get; } = Path.Combine(env.ContentRootPath, "App_Data", "app-log.txt");
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        services.AddControllersWithViews();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostApplicationLifetime lifetime)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
-            Env = env;
-            LogPath = Path.Combine(env.ContentRootPath, @"App_Data", @"app-log.txt");
+            app.UseDeveloperExceptionPage();
         }
-
-        public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Env { get; }
-        private string LogPath { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        else
         {
-            services.AddControllersWithViews();
+            app.UseExceptionHandler("/Home/Error");
         }
+        app.UseStaticFiles();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        lifetime.ApplicationStarted.Register(OnAppStarted);
+
+        lifetime.ApplicationStopping.Register(OnAppStopping);
+
+        lifetime.ApplicationStopped.Register(OnAppStopped);
+
+        app.UseEndpoints(endpoints =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            lifetime.ApplicationStarted.Register(OnAppStarted);
-
-            lifetime.ApplicationStopping.Register(OnAppStopping);
-
-            lifetime.ApplicationStopped.Register(OnAppStopped);
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
 
 
-        public void OnAppStarted()
-        {
-            File.AppendAllText(LogPath, $"App started at {DateTime.Now}\r\n");
-        }
+    public void OnAppStarted()
+    {
+        File.AppendAllText(LogPath, $"App started at {DateTime.Now}\r\n");
+    }
 
 
-        public void OnAppStopping()
-        {
-            File.AppendAllText(LogPath, $"App stopping at {DateTime.Now}\r\n");
-        }
+    public void OnAppStopping()
+    {
+        File.AppendAllText(LogPath, $"App stopping at {DateTime.Now}\r\n");
+    }
 
 
-        public void OnAppStopped()
-        {
-            File.AppendAllText(LogPath, $"App stopped at {DateTime.Now}\r\n");
-        }
+    public void OnAppStopped()
+    {
+        File.AppendAllText(LogPath, $"App stopped at {DateTime.Now}\r\n");
     }
 }
